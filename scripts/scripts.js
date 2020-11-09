ca81eb5d21dbf121193742f79b3e6fd3f874dabd
Joyce Liu <joyliu84@hamiltonchristian,school.nz>
2020-11-09 15:06:12 +1300

    added js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,49 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+let allQuestions = []
+
+const submit = document.querySelector('#send_button')
+submit.addEventListener('click', async () => {
+
+   let userInput = document.querySelector('#input_field').value
+  
+   let answer = await processAnswer(userInput)
+
+   console.log(answer)
+
+   let msgbox = document.querySelector("#Message_list")
+
+   msgbox.innerHTML += `
+     <div class="q_a_container">
+        <ul class="question">${userInput}</ul>
+        <ul class="answer">${answer}</ul>
+     </div>`
+
+  })
+
+  async function processAnswer(question) {
+
+    let answer = await fetch('../scripts/data.json')
+    .then(res => res.json())
+    .then(data => {
+
+      let allWords = question.split(" ")
+
+      console.log(allWords)
+
+      let allQuestions = data.filter(d => {
+        console.log(d.question.indexOf(question))
+        return d.question.indexOf(question) > -1
+      })
+
+      const answer = allQuestions[0] != null ? allQuestions[0].answer.map(a => `<il>${a}<il>`).join("") :
+      "<il>No Answer</il>"
+
+      return `${answer}`
+
+    })
+
+    return answer
+  }
